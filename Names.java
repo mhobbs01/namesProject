9fcb68d87725e246ac6466d3e04da8190e97eaef
ntinsley78 <atinsley@carthage.edu>
2014-08-04 21:22:36 -0500

    Added printNickTinsley method
    added method inside Names class to print my full name and then called it
    from the driver.

--- Names.java
+++ Names.java
@@ -3,6 +3,15 @@ public class Names
 	public static void main(String args[])
 	{
 		System.out.println("Mark Robert Mahoney");
+        //to print out my full name from the system line
         System.out.println("Alfred Nicholas Tinsley");
+
+        printNickTinsley();
+	}
+
+	//method to print out nick tinsley from within the Names class
+	private void printNickTinsley()
+	{
+		System.out.println("Alfred Nicholas Tinsley")
 	}
 }
