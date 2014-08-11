71f60746b008c436f2d28df0aa30ad64e7fa6177
Michael Hobbs <mhobbs@carthage.edu>
2014-08-11 11:53:19 -0500

    remove main,add new main with println

--- Names.java
+++ Names.java
@@ -1,8 +1,9 @@
 public class Names
 {
-	public static void main(String args[])
-	{
-		System.out.println("Mark Robert Mahoney");
-		System.out.println("Michael Richard Hobbs");
+	
+	public static void main(String args[]){
+			
+		System.out.println("Hi");
 	}
+
 }
