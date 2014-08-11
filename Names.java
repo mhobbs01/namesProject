3a453b44dba0c0e903a7eefe112752c1a338289d
Michael Hobbs <mhobbs@carthage.edu>
2014-08-11 13:54:17 -0500

    added new print_names function removed println "hi"

--- Names.java
+++ Names.java
@@ -1,9 +1,15 @@
 public class Names
 {
-	
+	void print_names() {
+				
+				System.out.println("Michael Richard Hobbs");
+				System.out.println("Mark Robert Mahoney");
+				
+			}
 	public static void main(String args[]){
-			
-		System.out.println("Hi");
+		Names name1 = new Names();
+		name1.print_names();
+	
 	}
 
 }
