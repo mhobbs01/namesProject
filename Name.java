d0a2f5c057957ea133d5a3ecce972dd46b6556fb
Michael Hobbs <mhobbs@carthage.edu>
2014-08-11 14:14:24 -0500

    added the Name class with name printed in constr

---
+++ Name.java
@@ -0,0 +1,8 @@
+public class Name 
+{
+	public Name()
+	{
+		System.out.println("Michael Richard Hobbs");
+	}
+
+}
--- Names.java
+++ Names.java
@@ -1,15 +1,18 @@
+
 public class Names
 {
-	void print_names() {
+	void print_names() 
+	{
 				
-				System.out.println("Michael Richard Hobbs");
-				System.out.println("Mark Robert Mahoney");
+		System.out.println("Michael Richard Hobbs");
+		System.out.println("Mark Robert Mahoney");
 				
-			}
+	}
 	public static void main(String args[]){
 		Names name1 = new Names();
 		name1.print_names();
-	
+		Name name2 = new Name();
+			
 	}
 
 }
